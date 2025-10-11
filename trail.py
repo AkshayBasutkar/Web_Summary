353a021900e108a1adb4d62aa6a398e95275844d
Akshay Basutkar <100553716+AkshayBasutkar@users.noreply.github.com>
2025-10-11 13:40:03 +0530

    Delete trail.py

--- trail.py
+++
@@ -1,5 +0,0 @@
-from googleapiclient.discovery import build
-service = build("customsearch", "v1", developerKey="AIzaSyA7k7eZAzVdsm1cWDGDlk2IQFINiTXMAf8")
-res = service.cse().list(q="Tiger", cx="b1fd71c797e6043d5", num=3).execute()
-for item in res.get("items", []):
-    print(item["link"])
